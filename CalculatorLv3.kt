c0b9a149b30289c88d45c74770d3eafb12ad8e4e
bonggyulim <qhdrbdhkdwks@gmail.com>
2024-03-08 19:54:36 +0900

    수정 완료

--- CalculatorLv3.kt
+++ CalculatorLv3.kt
@@ -8,11 +8,11 @@ class CalculatorLv3 {
     open class Calculator() {
 
         fun mainAct(){
-            var add = AddOperation()                          // 더하기 연산
-            var sub = SubstractOperation()                    // 빼기 연산
-            var mul = MultiplyOperation()                     // 곱하기 연산
-            var div = DivideOperation()                       // 나누기 연산
-            var splitFourData = splitFourData()
+            var add = AddOperation()                                        // 더하기 연산
+            var sub = SubstractOperation()                                  // 빼기 연산
+            var mul = MultiplyOperation()                                   // 곱하기 연산
+            var div = DivideOperation()                                     // 나누기 연산
+            var splitFourData = splitFourData()                             // 입력된 문자 자르기
 
             println("계산할 식을 작성해주세요(숫자,연산자,숫자,등호) : ")
 
@@ -20,8 +20,8 @@ class CalculatorLv3 {
             var list = splitFourData.split(inputStr)
             var result = 0
 
-            if(list.get(3) == 1) {
-                when (list.get(1)) {
+            if(list.get(3) == 1) {                                          // 마지막입력이 = 이면 실행
+                when (list.get(1)) {                                        // 들어온 연산자의 연산을 실행
                     1 -> result = add.operation(list.get(0), list.get(2))
                     2 -> result = sub.operation(list.get(0), list.get(2))
                     3 -> result = mul.operation(list.get(0), list.get(2))
@@ -35,7 +35,7 @@ class CalculatorLv3 {
             re(result)
         }
 
-        fun re(result_: Int): Int {
+        fun re(result_: Int): Int {                                         // 재귀함수를 써 여러번 연산 가능
             var result = 0
             var add = AddOperation()
             var sub = SubstractOperation()
@@ -62,7 +62,7 @@ class CalculatorLv3 {
         }
     }
 
-    abstract class Operation(): Calculator() {
+    abstract class Operation(): Calculator() {                                  // 연산 추상클래스
         open var result = 0
         open fun operation(num1: Int, num2: Int): Int {
             return result
@@ -98,7 +98,7 @@ class CalculatorLv3 {
     }
 
 
-    abstract class SplitData(): Calculator() {
+    abstract class SplitData(): Calculator() {                                  // 데이터 분할 추상 클래스
         var listInt = mutableListOf<Int>()
         var num1: Int = 0
         var num2: Int = 0
